746857114a7a8c1926ddbdd7aa92e93fc13d266f
FGRANET <124205202+FGRANET@users.noreply.github.com>
2024-03-15 18:23:59 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ from src.data.load_data import load_csv_data
 from src.data.clean_data import dropna, drop_duplicates
 from src.data.split_data import split_data
 from src.data.preprocess_data import standardize_data
-from src.features.feature_engineering import select_k_best_features
+from features.select_features import select_features_kbest,select_features_select_from_model,select_features_rfe
 
 
 parser = argparse.ArgumentParser(description="Pipeline d'exécution pour le projet DEFAUTS_PLAQUES_ACIER")
@@ -12,8 +12,9 @@ parser.add_argument('--load-data', action='store_true', help="Charge les donnée
 parser.add_argument('--clean-data', action='store_true', help="Nettoie les données chargés (doublons, valeurs manquantes)")
 parser.add_argument('--split-data',action='store_true', help="Séparation du train en train et test")
 parser.add_argument('--preprocess-data',action='store_true', help="Standardisation ou équivalent")
-parser.add_argument('--feature-engineering',action='store_true', help="Création features / Réduction de dimension")
-
+parser.add_argument('--feature-engineering',action='store_true', help="Création features")
+parser.add_argument('--select-features', action='store_true', help="Activer la sélection de caractéristiques")
+parser.add_argument('--method', type=str, default='select_from_model', choices=['select_kbest','select_from_model', 'rfe'], help="Méthode de sélection de caractéristiques à utiliser")
 # Ajoutez d'autres arguments selon les besoins
 args = parser.parse_args()
 
@@ -36,8 +37,16 @@ def main():
     if args.preprocess_data:
         X_train,X_test= standardize_data(X_train,X_test)
     
-    if args.feature_engineering:
-        X_train,X_test = select_k_best_features(X_train, y_train, X_test, k=10)
+    if args.select_features:
+        if args.method =='select_kbest':
+            X_train, X_test = select_features_kbest(X_train, y_train, X_test, k=10)
+        if args.method == 'select_from_model':
+            X_train, X_test, selected_features = select_features_select_from_model(X_train,X_test, y_train, model=None, threshold='mean')
+        elif args.method == 'rfe':
+            X_train, X_test, selected_features = select_features_rfe(X_train,X_test, y_train, n_features_to_select=10, model=None)
+        print(f"Caractéristiques sélectionnées : {selected_features}")
+    else:
+        X_train = X_train
 
 
 
