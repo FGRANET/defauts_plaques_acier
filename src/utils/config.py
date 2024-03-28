d851fee391397920e6b392cf5024493a786ec768
FGRANET <124205202+FGRANET@users.noreply.github.com>
2024-03-28 17:06:24 +0100

    Create config.py

---
+++ src/utils/config.py
@@ -0,0 +1,8 @@
+import json
+
+def load_config(config_path):
+    with open(config_path, 'r') as config_file:
+        config = json.load(config_file)
+    return config
+
+config=load_config("random_forest_randomsearch.json")
